f4c2001a0b2a9bc694cf925dfbc1b31905c75f41
PedroQWQ <116931006+PedroQWQ@users.noreply.github.com>
2024-02-01 15:14:01 -0600

    initial commit

---
+++ alumno.js
@@ -0,0 +1,8 @@
+export class Alumno{
+    constructor (nombre,apellidoP,apellidoM,cal){
+        this.nombre=nombre;
+        this.apellidoP=apellidoP;
+        this.apellidoM=apellidoM;
+        this.cal=cal;
+    }
+}
