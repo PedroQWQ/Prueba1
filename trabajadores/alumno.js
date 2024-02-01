e869949fb61700dc7e5f197284a7abe0eb9719e4
PedroQWQ <116931006+PedroQWQ@users.noreply.github.com>
2024-02-01 15:15:11 -0600

    Delete trabajadores directory

--- trabajadores/alumno.js
+++
@@ -1,8 +0,0 @@
-export class Alumno{
-    constructor (nombre,apellidoP,apellidoM,cal){
-        this.nombre=nombre;
-        this.apellidoP=apellidoP;
-        this.apellidoM=apellidoM;
-        this.cal=cal;
-    }
-}
